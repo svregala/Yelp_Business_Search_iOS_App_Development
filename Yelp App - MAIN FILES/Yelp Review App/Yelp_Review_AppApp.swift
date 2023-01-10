3e09058dabc2e8e8d06a4b80c94f8dbd499ec9bd
Steve Regala <sregala@usc.edu>
2023-01-09 16:41:24 -0800

    Yelp App

---
+++ Yelp App - MAIN FILES/Yelp Review App/Yelp_Review_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yelp_Review_AppApp.swift
+//  Yelp Review App
+//
+//  Created by Steve Regala on 12/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Yelp_Review_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Yelp App - MAIN FILES/Yelp Review AppUITests/Yelp_Review_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Yelp_Review_AppUITestsLaunchTests.swift
+//  Yelp Review AppUITests
+//
+//  Created by Steve Regala on 12/2/22.
+//
+
+import XCTest
+
+final class Yelp_Review_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
